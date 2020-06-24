22e591530e210ba18cbf1f682fe9ac4c517a0da1
Herteg Kohar <koha5160@mylaurier.ca>
2020-06-24 18:42:26 -0400

    Added previous as parameter to aux function and took off parent connection from nodes

--- Maximum_Heap_Linked.py
+++ Maximum_Heap_Linked.py
@@ -1,19 +1,9 @@
 from copy import deepcopy
 class _MH_Node:
-    def __init__(self,_parent,value):
+    def __init__(self,value):
         self._value=value
-        self._parent=_parent
         self._right=None
         self._left=None
-        
-    def _set_parent(self):
-        if self._left is not None and self._right is not None:
-            self._left._parent=self
-            self._right._parent=self
-        elif self._left is not None:
-            self._left._parent=self
-        elif self._right is not None:
-            self._right._parent=self
         
 class Maximum_Heap_Linked:
     def __init__(self):
@@ -29,88 +19,90 @@ class Maximum_Heap_Linked:
 
     def push(self,value):
         if self._root is None:
-            node=_MH_Node(None,value)
+            node=_MH_Node(value)
             self._root=node
             self._count+=1
             self._last=node
-
         else:
-            self._root,inserted=self._push_aux([self._root],value)
+            self._root,inserted=self._push_aux([self._root],None,value)
         
 
-    def _push_aux(self,q,value):
+    def _push_aux(self,q,previous,value):
         inserted=False
         node=q.pop(0)
         if not node._left: 
-            temp=_MH_Node(node,value)
+            temp=_MH_Node(value)
             node._left=temp
             self._last=temp
+            self._count+=1
             inserted=True
 
         else:
             q.append(node._left)
 
         if not node._right and not inserted:
-            temp=_MH_Node(node,value)
+            temp=_MH_Node(value)
             node._right=temp
             self._last=temp
+            self._count+=1
             inserted=True
         elif not inserted:
             q.append(node._right)
         if not inserted:
-            self._push_aux(q,value)
+            self._push_aux(q,node,value)
 
         if node._left is not None and node._left._value>node._value:
-            node=self._swap_left(node)
+            node=self._swap_left(node,previous)
 
         elif node._right is not None and node._right._value>node._value:
-            node=self._swap_right(node)
+            node=self._swap_right(node,previous)
         return node,inserted
 
-    def _swap_left(self,parent):
+    def _swap_left(self,parent,previous):
         updated=parent._left
         temp_right=updated._right
         updated._right=parent._right
-        parent._right=temp_right
         parent._left=updated._left
+        parent._right=temp_right
         updated._left=parent
-        updated._parent=parent._parent
-        parent._parent=updated
-        updated._set_parent()
-        parent._set_parent()
-        if updated._parent is not None:
-            if updated._parent._left is parent:
-                updated._parent._left=updated
+        if previous is not None:
+            if previous._left is parent:
+                previous._left=updated
             else:
-                updated._parent._right=updated
+                previous._right=updated
+                
         return updated
         
-    def _swap_right(self,parent):
+    def _swap_right(self,parent,previous):
         updated=parent._right
         temp_left=updated._left
         updated._left=parent._left
-        parent._left=temp_left
         parent._right=updated._right
+        parent._left=temp_left
         updated._right=parent
-        updated._parent=parent._parent
-        parent._parent=updated
-        updated._set_parent()
-        parent._set_parent()
-        if updated._parent is not None:
-            if updated._parent._left is parent:
-                updated._parent._left=updated
+        if previous is not None:
+            if previous._right is parent:
+                previous._right=updated
             else:
-                updated._parent._right=updated
+                previous._left=updated
         return updated
 
     def peek(self):
         assert self._root is not None, "Can't peek at an empty heap"
         return self._root._value
-        
-MH=Maximum_Heap_Linked()
-MH.push(1)
-MH.push(2)
-MH.push(3)
-MH.push(4)
-MH.push(5)
-print(MH.peek())
+    
+    def levelorder(self):
+        nodes = []
+        if self._root is not None:
+            queue = []
+            queue.append(self._root)
+
+            while len(queue) > 0:
+                node = queue.pop(0)
+                nodes.append(deepcopy(node._value))
+
+                if node._left is not None:
+                    queue.append(node._left)
+                if node._right is not None:
+                    queue.append(node._right)
+        return nodes
