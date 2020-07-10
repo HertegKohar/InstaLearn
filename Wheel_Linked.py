fdffc0cb1201f90b5716749dedf9159bcd933666
Herteg Kohar <koha5160@mylaurier.ca>
2020-07-10 17:32:24 -0400

    Fixed remove and added a helper function to reset

--- Wheel_Linked.py
+++ Wheel_Linked.py
@@ -14,6 +14,21 @@ class Wheel:
 	def is_empty(self):
 		return self._count==0
 
+	def clear(self):
+		self._front=None
+		self._rear=None
+		self._current=None
+		self._count=0
+		self._prev=None
+	
+	def _check_reset(self):
+		if self._count==0:
+			self._front=None
+			self._rear=None
+			self._current=None
+			self._prev=None
+
+
 	def __len__(self):
 		return self._count
 
@@ -29,11 +44,11 @@ class Wheel:
 		self._count+=1
 
 	def peek(self):
-		assert self._front is not None, "Can't peek at an empty wheel"
+		assert self._front, "Can't peek at an empty wheel"
 		return self._current._value
 
 	def pop(self):
-		assert self._front is not None, "Can't pop an empty wheel"
+		assert self._front, "Can't pop an empty wheel"
 		if self._prev is None:
 			value=self._front._value
 			self._front=self._front._next
@@ -47,54 +62,44 @@ class Wheel:
 			self._current=self._current._next
 			self._prev._next=self._current
 		self._count-=1
-		if self._count==0:
-			self._front=None
-			self._rear=None
-			self._current=None
-			self._prev=None
+		self._check_reset()
 		return value
 
 	def get_next(self):
-		assert self._front is not None, "Can't get the next of an empty wheel"
+		assert self._front, "Can't get the next of an empty wheel"
 		self._prev=self._current
 		self._current=self._current._next
 		return self._current._value
 
 	def remove(self,key):
-		assert self._front is not None, "Can't remove from an empty wheel"
+		assert self._front, "Can't remove from an empty wheel"
 		value=None
-		if self._front._value==key and self._current is self._front:
+		if self._front._value==key and self._front is self._rear:
+			value=self._front._value
+		elif self._front._value==key:
 			value=self._front._value
 			self._front=self._front._next
-			self._current=self._front
-			if self._prev is not None:
-				self._rear=self._prev
-				self._prev._next=self._current
-		elif self._rear._value==key and self._current is self._rear:
-			value=self._rear._value
-			self._current=self._current._next
-			self._rear=self._prev
 			self._rear._next=self._front
+		if self._current._value==key:
+			value=self._current._value
+			self._current=self._current._next
+			if self._prev:
+				self._prev._next=self._current
 		else:
 			prev=None
 			current=self._front
 			while current is not self._rear and current._value!=key:
 				prev=current
 				current=current._next
-			if current is self._front and current._value==key:
-				value=self._front._value
+			if current._value==key and current is self._rear:
+				value=self._rear._value
 				self._rear=prev
 				self._rear._next=self._front
 			elif current._value==key:
 				value=current._value
 				prev._next=current._next
-		if value is not None: self._count-=1
-		if self._count==0:
-			self._front=None
-			self._rear=None
-			self._current=None
-			self._prev=None
-		return value
+		if value: self._count-=1
+		self._check_reset()
 
 	def __str__(self):
 		current=self._front
