521c50817c9b01aea9b9988835554cc8a8888b56
Herteg Kohar <koha5160@mylaurier.ca>
2020-07-29 21:20:11 -0400

    Testing sign in and cronjob

--- InstaLearnApp/app.py
+++ InstaLearnApp/app.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI, Request, BackgroundTasks, Depends
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
-from crontab import Crontab
+import os
+from crontab import CronTab
+
 #Create a FastAPI instance
 app=FastAPI()
 
-class AddUser(BaseModel):
+class SignIn(BaseModel):
 	username: str
+	password: str
 
 templates=Jinja2Templates(directory='templates')
 
@@ -15,14 +18,33 @@ templates=Jinja2Templates(directory='templates')
 def home(request: Request):
 	return templates.TemplateResponse('home.html',{'request':request})
 
-@app.post("/user")
+@app.get("/user")
 async def add_user(request: Request, background_tasks:BackgroundTasks):
-	background_tasks.add_task(test_notification)
 	return {"code":"success"}
 
-@app.get('/run')
-async def run_cron(request: Request, background_tasks: BackgroundTasks):
-	pass
+def cronjob():
+	with CronTab(user=os.environ.get('user')) as cron:
+		job=cron.new(command='/usr/bin/python3 /home/{:s}/cronjob.py'.format(os.environ.get('user')))
+		job.minute.every(1)
+
+
+@app.post('/run')
+async def run_cron(signin_request: SignIn, background_tasks: BackgroundTasks):
+	if signin_request.username==os.environ.get('username') and signin_request.password==os.environ.get('password'):
+		background_tasks.add_task(cronjob)
+		return {"code":"success"}
+	return {"code":"failed"}
+
+@app.get('/status')
+async def run_cron(signin_request: SignIn, background_tasks: BackgroundTasks):
+	if signin_request.username==os.environ.get('username') and signin_request.password==os.environ.get('password'):
+		pass
+	return 
+
+
+
+
+
 
 
 	
--- InstaLearnApp/cronjob.py
+++ InstaLearnApp/cronjob.py
@@ -1,9 +1,2 @@
-import sys
-import os
-os.chdir('..')
-sys.path.append(os.getcwd())
-os.chdir("InstaLearnApp")
-from InstaLearnPackage import bot
-#bot.monitor_users()
-for user in bot.users: print(user)
-bot.save_bot()
+from InstaDataPackage import bot
+for user in bot.users:print(user)
