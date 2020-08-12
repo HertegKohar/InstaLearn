a78613c1d4e3aee807887ed54879f1f3dceb628c
Herteg Kohar <koha5160@mylaurier.ca>
2020-08-12 19:37:24 -0400

    Renamed folder to tests

---
+++ tests/test_app.py
@@ -0,0 +1,16 @@
+import os
+import unittest
+
+import requests
+
+
+class TestApp(unittest.TestCase):
+    def test_status(self):
+        json_data = {
+            "username": os.environ["username"],
+            "password": os.environ["password"],
+        }
+        response = requests.post(
+            url="{var['ip']}:{var['port']}".format(var=os.environ), json=json_data
+        )
+        raise NotImplementedError
