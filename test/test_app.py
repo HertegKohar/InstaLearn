019e65bc8da44f8fa098de6d4b93fb9af788c6d4
Herteg Kohar <koha5160@mylaurier.ca>
2020-08-14 14:14:02 -0400

    Implemented Simple Status Test

--- test/test_app.py
+++ test/test_app.py
@@ -5,12 +5,27 @@ import requests
 
 
 class TestApp(unittest.TestCase):
-    @unittest.skip("Status Response Test Not Implemented Yet")
     def test_status(self):
         json_data = {
             "username": os.environ["username"],
             "password": os.environ["password"],
         }
         response = requests.post(
-            url="{var['ip']}:{var['port']}".format(var=os.environ), json=json_data
+            url="http://{var[ip]}:{var[port]}/status".format(var=os.environ),
+            json=json_data,
         )
+        if response.json()["code"] == "fail":
+            self.fail("Unable to get status")
+
+    def test_add_user(self):
+        json_data = {
+            "username": os.environ.get("username"),
+            "password": os.environ.get("password"),
+        }
+        response = requests.post(
+            url="http://{var[ip]}:{var[port]}/add_user".format(var=os.environ),
+            json=json_data,
+        )
+        if response.json()["code"] == "success":
+            self.fail("Root user added again")
+
