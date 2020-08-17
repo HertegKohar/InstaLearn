e36ab4729e158077214fc38e827cffdd2ea8518a
Herteg Kohar <koha5160@mylaurier.ca>
2020-08-17 18:56:50 -0400

    Added simple tests for context managers

--- test/test_database.py
+++ test/test_database.py
@@ -1,18 +1,28 @@
 import unittest
+from sqlite3 import Connection, ProgrammingError
 
 from mysql.connector.connection import MySQLConnection
+from mysql.connector.errors import OperationalError
 
-from InstaDataPackage import Instabase
+from InstaDataPackage.Instabase import DB_Session, DB_Session_Local
 
 
 class TestDBase(unittest.TestCase):
     def test_connection(self):
-        connection = Instabase._connect_db()
-        self.assertIsInstance(connection, MySQLConnection)
-        connection.close()
+        with DB_Session() as db:
+            self.assertIsInstance(db._DB_Session__connection, MySQLConnection)
+        with self.assertRaises(OperationalError):
+            db._DB_Session__connection.cursor()
+
+    def test_connection_local(self):
+        with DB_Session_Local() as db:
+            self.assertIsInstance(db._DB_Session_Local__connection, Connection)
+        with self.assertRaises(ProgrammingError):
+            db._DB_Session_Local__connection.cursor()
 
     def test_entries(self):
-        result = Instabase.show_all()
-        self.assertIsInstance(result, list)
+        with DB_Session() as db:
+            result = db.show()
+            self.assertIsInstance(result, list)
         if not result:
             self.fail("No entries in training table are apprearing")
